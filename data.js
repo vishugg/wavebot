0f111604c53e0bc519d3ec6ee7534fb88e175c99
vishugg <88967649+vishugg@users.noreply.github.com>
2022-02-03 14:59:26 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,33 +2,33 @@ module.exports = {
 
 mongoLink:'mongodb+srv://nft:vishu@paid1.qldpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
 
-bot_token:'5298373459:AAEMNeqTL_Vs9tulBHLcj5n0nl_6PwUwdOU',//bot token
+bot_token:'5182443040:AAG-AhJdvX3207uoqCX35PgXnoAyPQTCdQQ',//bot token
 
-bot_name:'WavesInstantAirdropBot',//bot username without @
+bot_name:'RiseINUAirdrop_bot',//bot username without @
 
 bot_admin:1891485685,// admin telegram id
 
-channelsList : ['@AirdropBen' , '@OfficialMrAirdrops' , '@TheAirdropX' , '@The0xBulls','@AirdropV_Official','@AirdropProvince'],// channels list
+channelsList : [‘@AirdropBen’ , ‘@OfficialMrAirdrops’ , ‘@TheAirdropX’ , ‘@Airdrops_Earner’ , ‘@MrX_Crypto’ , ‘@Cryptoverse_shortnews’],// channels list
 
-channelscheck : ['@AirdropBen','@AirdropV_Official','@Payout_X','@OfficialMrAirdrops','@TheAirdropX','@The0xBulls','@AirdropProvince'],
+channelscheck : ‘@AirdropBen’ , ‘@OfficialMrAirdrops’ , ‘@TheAirdropX’ , ‘@Airdrops_Earner’ , ‘@MrX_Crypto’ , ‘@Cryptoverse_shortnews’ , '@RiseINUPayments'],
 
 dp: '*Join @TheAirdropX For Legit Airdrops Distribution Guaranteed By Team AirdropX*',
 
 exp:'https://wavesexplorer.com/tx/',
 
-reffer_bonus:0.002,// refferal bonus amount
+reffer_bonus: 67000000,// refferal bonus amount
 
-min_wd:0.006,//min withdrawal amount
+min_wd: 20000000,//min withdrawal amount
 
 daily_bonus:00000000000,//,// daily bonus amount
 
-currency:'WAVES', //bot cuurency
+currency:'RiseINU', //bot cuurency
 
 key:'a1c32c2ee1261bd5cdfc1a9a6f9446d7e4586b3f64fb9bb9215509122a3fb653',
 
 secret:'e00C6f898fBb172400D4E6D7cAB67bFb4980e989D0471Deb703E87EA8D6c0287',
 
-payment_channel:'@Payout_X',// payment channel username
+payment_channel:'@RiseINUPayments',// payment channel username
 
 tw1:'http://twitter.com/CryptoVerse_Co',
 
