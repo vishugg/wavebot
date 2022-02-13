ba2f39d67f61cc429562d843726d09b2f35b07f4
vishugg <88967649+vishugg@users.noreply.github.com>
2022-02-13 18:42:37 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,35 +2,35 @@ module.exports = {
 
 mongoLink:'mongodb+srv://nft:vishu@paid1.qldpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
 
-bot_token:'5182443040:AAFdJzkOmT1B8-ux607Oz2GYDTEPHehV6iA',//bot token
+bot_token:'5111485924:AAHq08l3crOJnmzK325tH3pRrPY6M8NaTxY',//bot token
 
-bot_name:'RiseINUAirdrop_bot',//bot username without @
+bot_name:'SpaceBabyTokenAirdrop_bot',//bot username without @
 
 bot_admin:1891485685,// admin telegram id
 
-channelsList : ['@AirdropBenGroup' , '@OfficialMrAirdrops' , '@TheAirdropX' , '@AirdropBen', '@Airdrops_Earner' , '@MrCryptoXCommunity' , '@CryptoVerse_Exclusive'],// channels list
+channelsList : ['@airdrop_market_pro_news' , '@airdrop_NFT1' , '@airdrop_market_pro' , '@NFTs_Exclusive', '@CryptoVerse_Exclusive'],// channels list
 
-channelscheck : ['@AirdropBen' , '@OfficialMrAirdrops' , '@TheAirdropX' , '@Airdrops_Earner'  , '@CryptoVerse_Exclusive' , '@RiseINUPayments'],
+channelscheck : ['@airdrop_market_pro_news' , '@airdrop_NFT1' , '@airdrop_market_pro' , '@NFTs_Exclusive', '@SPBPaymentProofs' , '@CryptoVerse_Exclusive'],
 
-payment_channelz:'@hdjdjjfjfjjf',
+payment_channelz:'@PayOfSPB',
 
 dp: '*Join @TheAirdropX For Legit Airdrops Distribution Guaranteed By Team AirdropX*',
 
 exp:'https://wavesexplorer.com/tx/',
 
-reffer_bonus: 90000000,// refferal bonus amount
+reffer_bonus: 5000,// refferal bonus amount
 
-min_wd: 20000000,//min withdrawal amount
+min_wd: 20000,//min withdrawal amount
 
 daily_bonus:10000000000000000000000000,//,// daily bonus amount
 
-currency:'RiseINU', //bot cuurency
+currency:'SPB', //bot cuurency
 
 key:'a1c32c2ee1261bd5cdfc1a9a6f9446d7e4586b3f64fb9bb9215509122a3fb653',
 
 secret:'e00C6f898fBb172400D4E6D7cAB67bFb4980e989D0471Deb703E87EA8D6c0287',
 
-payment_channel:'@RiseINUPayments',// payment channel username
+payment_channel:'@SPBPaymentProofs',// payment channel username
 
 tw1:'http://twitter.com/CryptoVerse_Co',
 
